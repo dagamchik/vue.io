5e921b3d8dc70bb6eca5609ce897bbd2e01dfa26
dagamchik <dagamchik@gmail.com>
2021-05-31 13:48:28 +0500

    add 4.3 hw

---
+++ src/components/BasePagination.vue
@@ -0,0 +1,64 @@
+<template>
+  <ul class="catalog__pagination pagination">
+    <li class="pagination__item">
+      <a v-if="page <= pages && page > 1" href="#"
+      class="pagination__link pagination__link--arrow"
+      aria-label="Предыдущая страница" @click.prevent="paginate(page - 1)"
+      :class="{'pagination__link--disabled': page === 1}">
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-left"></use>
+        </svg>
+      </a>
+      <a v-else-if="page === 1" href="#" class="pagination__link pagination__link--arrow"
+      aria-label="Предыдущая страница" @click.prevent
+      :class="{'pagination__link--disabled': page === 1}">
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-left"></use>
+        </svg>
+      </a>
+    </li>
+    <li class="pagination__item" v-for="pageNumber in pages" :key="pageNumber">
+      <a href="#" class="pagination__link"
+      :class="{'pagination__link--current': pageNumber === page}"
+      @click.prevent="paginate(pageNumber)">
+        {{ pageNumber }}
+      </a>
+    </li>
+    <li class="pagination__item">
+      <a v-if="page < pages" class="pagination__link pagination__link--arrow" href="#"
+      aria-label="Следующая страница" @click.prevent="paginate(page + 1)"
+      :class="{'pagination__link--disabled': page === pages}">
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-right"></use>
+        </svg>
+      </a>
+      <a v-else class="pagination__link pagination__link--arrow" href="#"
+      aria-label="Следующая страница" @click.prevent
+      :class="{'pagination__link--disabled': page === pages}">
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'page',
+    event: 'paginate',
+  },
+  props: ['page', 'count', 'perPage'],
+  computed: {
+    pages() {
+      return Math.ceil(this.count / this.perPage);
+    },
+  },
+  methods: {
+    paginate(page) {
+      this.$emit('paginate', page);
+    },
+  },
+};
+</script>
