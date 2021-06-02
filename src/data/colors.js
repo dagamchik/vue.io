fa3925ee7a13aae07ecdc057f9b2bb6d29d3f770
dagamchik <dagamchik@gmail.com>
2021-06-02 20:45:27 +0500

    colors

---
+++ src/data/colors.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    blueColor: '#73B6EA',
+  },
+  {
+    yellowColor: '#FFBE15',
+  },
+  {
+    grayColor: '#939393',
+  },
+  {
+    lightGreenColor: '#8BE000',
+  },
+  {
+    orangeColor: '#FF6B00',
+  },
+  {
+    whiteColor: '#FFFFFF',
+  },
+  {
+    blackColor: '#000',
+  },
+];
